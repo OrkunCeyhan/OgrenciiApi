234938b11442c8c120bae086da015cfb880849d2
OrkunCeyhan <ceyhanorkun@gmail.com>
2023-09-04 07:24:49 +0300

    OgrenciApi
    Basit bir api ayağa kaldırma işlemi

---
+++ OgrenciApi/WebApplication5/Controllers/OgrenciController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OgrenciController : ControllerBase
+    {
+        static List<Ogrenci> ogrenciler = new List<Ogrenci>
+        {
+            new Ogrenci { Id = 1, AdSoyad = "ahmet sarı" },
+            new Ogrenci { Id = 2, AdSoyad = "mehmet köse" },
+            new Ogrenci { Id = 3, AdSoyad = "salih vardar" }
+        };
+
+        [HttpGet]
+        public List<Ogrenci> Get()
+        {
+
+            return ogrenciler;
+        }
+
+        [HttpGet("{id}")]
+        public Ogrenci Get(int id) {
+        return ogrenciler.FirstOrDefault(o => o.Id == id);
+        }
+        [HttpPost]
+        public Ogrenci Post(Ogrenci ogrenci) {
+            ogrenciler.Add(ogrenci);
+            return ogrenci;
+        }
+
+    }
+}
---
+++ OgrenciApi/WebApplication5/Models/Ogrenci.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication5.Models
+{
+    public class Ogrenci
+    {
+        public int Id { get; set; }
+        public string AdSoyad { get; set; }
+    }
+}
